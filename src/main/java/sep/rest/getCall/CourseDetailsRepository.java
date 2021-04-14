700c4d9ac67b1e4692d4c8cc252b7127a13f832d
smsuhath <slmsuhath93@gmail.com>
2021-04-14 10:23:36 +0530

    Update CourseDetailsRepository.java
    Add custom query in interface

--- src/main/java/sep/rest/getCall/CourseDetailsRepository.java
+++ src/main/java/sep/rest/getCall/CourseDetailsRepository.java
@@ -3,5 +3,15 @@ package sep.rest.getCall;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseDetailsRepository extends JpaRepository<CourseDetails, String> {
+  
+  @Query("SELECT b FROM CourseDetails b where b.unid = ?1")
+	List<CourseDetails> findByUniId(String uniId);
+	
+	@Query("SELECT b FROM CourseDetails b where b.unid = ?1 AND b.userid = ?2")
+	CourseDetails findByUniIdAndUserId(String uniId, String userId);
+	
+	@Query("SELECT b FROM CourseDetails b where b.unid = ?1 AND b.userid = ?2")
+	List<CourseDetails> findByUniIdAndUserIdtt(String uniId, String userId);
+	
 
 }
