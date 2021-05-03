c5cd496ef54919b42e2ffc911b2edbeeddd2444a
smsuhath <slmsuhath93@gmail.com>
2021-05-03 19:49:45 +0530

    Update CourseDetailsRepository.java
    Add Filtration based on CourseFees

--- src/main/java/sep/rest/getCall/CourseDetailsRepository.java
+++ src/main/java/sep/rest/getCall/CourseDetailsRepository.java
@@ -13,5 +13,8 @@ public interface CourseDetailsRepository extends JpaRepository<CourseDetails, St
 	@Query("SELECT b FROM CourseDetails b where b.unid = ?1 AND b.userid = ?2")
 	List<CourseDetails> findByUniIdAndUserIdtt(String uniId, String userId);
 	
+	@Query("SELECT b FROM CourseDetails b WHERE b.coursefees <= ?1")
+	List<CourseDetails> findCourseByFees(float fees);
+	
 
 }
